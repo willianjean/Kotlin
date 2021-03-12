b19370612f3772b0753f5b5449743c13d5f2071d
Willian Jean <willianjean@gmail.com>
2021-03-12 18:15:37 -0400

    primeiro commit do estudo kotlin

---
+++ src/estudosKotlin.kt
@@ -0,0 +1,20 @@
+fun main() {
+    /*
+    println(Int.MAX_VALUE)
+    println(Float.MAX_VALUE)
+    println(Long.MAX_VALUE)
+    println(Byte.MAX_VALUE)
+    println(Short.MAX_VALUE)
+
+    //var camelCase
+    var currentAge = 22
+    var currentAgee:Int?
+    currentAgee = null
+    */
+
+    //val camelCase
+    val currentAge = 22
+    val currentAgee:Int?
+    currentAgee = null
+    println (currentAge)
+}
