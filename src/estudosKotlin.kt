2af2e934379579b4c96a2e74fd0f5244c19cb446
Willian Jean <willianjean@gmail.com>
2021-03-16 00:10:14 -0400

    estudos kotlin

--- src/estudosKotlin.kt
+++ src/estudosKotlin.kt
@@ -1,3 +1,6 @@
+const val MIN_AGE = 16
+const val MAX_AGE = 68
+
 fun main() {
     /*
     println(Int.MAX_VALUE)
@@ -10,11 +13,36 @@ fun main() {
     var currentAge = 22
     var currentAgee:Int?
     currentAgee = null
-    */
+
 
     //val camelCase
     val currentAge = 22
     val currentAgee:Int?
     currentAgee = null
     println (currentAge)
+    println (currentAgee)
+
+    //erro
+    var currentAge
+    currentAge = 22 //ERRO!
+
+    var currentYear = "Ano"
+    currentYear  = 2021 //ERRO!
+
+
+    var month:Int? = null
+    //Atribui valor null corretamente a month
+
+    var day:Int = null
+    // Erro de computação
+    // "Null can not be a value of a non-null type Int"
+    */
+
+    //Operadores Aritméticos
+    //a.plus(b) que atribuição é "a += b"
+    //a.minus(b) que atribuição é "a -= b"
+    //a.times(b) que atribuição é "a *= b"
+    // "a / b" é o mesmo que a.div(b) que atribuição é "a /=b"
+    // "a % b" é o mesmo que a.mod(b) que atribuição é "a%=b"
+    */
 }
