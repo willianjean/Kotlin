27bc998ebfd9e67dad0fda54b6e24b1b45bba4fd
Willian Jean <willianjean@gmail.com>
2021-03-19 19:12:07 -0400

    mais uma parte

--- src/estudosKotlin.kt
+++ src/estudosKotlin.kt
@@ -36,13 +36,61 @@ fun main() {
     var day:Int = null
     // Erro de computação
     // "Null can not be a value of a non-null type Int"
-    */
+
 
     //Operadores Aritméticos
-    //a.plus(b) que atribuição é "a += b"
-    //a.minus(b) que atribuição é "a -= b"
-    //a.times(b) que atribuição é "a *= b"
+    // a.plus(b) que atribuição é "a += b"
+    // a.minus(b) que atribuição é "a -= b"
+    // a.times(b) que atribuição é "a *= b"
     // "a / b" é o mesmo que a.div(b) que atribuição é "a /=b"
     // "a % b" é o mesmo que a.mod(b) que atribuição é "a%=b"
+
+    //Operadores comparativos
+    // Os comandos compareTo retornam os valores -1(menor que), 0(igual ou)
+    //1(maior). Já os operadores retornam valores booleanos;
+    // O compando equals retorna um booleano
+    // a.compareTo(b)
+    // a.equals(b)
+    // !a.equals(b)
+
+    //Operadores Lógicos
+    // E (&&) que é (expressão1) and (expressão2)
+    // Ou (||) que é (expressão1) or (expressão2)
+
+    //Operadores In e range
+    // contém (In)
+    // Não contem (!In)
+    // range/Faixa de valores (Int...Int)
+
+    val bingo = listOf(8,6,34,2,13)
+    var number = (1..34).random()
+    println(number)
+    println(number in bingo)
+
+    var age = (70..100).random()
+    println (age)
+    println(age in MIN_AGE..MAX_AGE)
+    println(age >= MIN_AGE && age <= MAX_AGE)
+
+    val s = "Olá, mundo!"
+
+    println(s[0])
+    println(s.first())
+    //imprime O (o caracter inicial do array)
+
+    println(s[s.length-1])
+    println(s.last())
+    //imprime ! (o caracter final do array)
     */
+
+    val name = "Ana"
+    val s = "Olá"
+
+    println(s + name)
+    //imprime OláAna
+    println("${s}, ${name}!")
+    //Imprime Olá, Ana!
+    println("Bem vinda(o), $name!")
+    //imprime Bem vinda(o), Ana!
+
 }
